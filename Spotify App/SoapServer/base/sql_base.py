f4fc9c62a08e4448570f22c281a427b7f51994d6
Chiriac Dan <dan-constantin.chiriac@student.tuiasi.ro>
2022-12-10 14:35:14 +0200

    Some changes

--- Spotify App/SoapServer/base/sql_base.py
+++ Spotify App/SoapServer/base/sql_base.py
@@ -3,5 +3,5 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-engine = create_engine('mariadb+mariadbconnector://admin:12341234@localhost:3306/IDM')
+engine = create_engine('mariadb+mariadbconnector://admin:12341234@localhost:3306/idm')
 Session = sessionmaker(bind=engine)
